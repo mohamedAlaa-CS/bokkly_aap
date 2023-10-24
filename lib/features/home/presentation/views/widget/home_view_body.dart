7876e72412f309109ea65337dd6e945c1aa37c57
Myproject <mohamed.alaa@gmail.com>
2023-10-24 00:06:17 -0700

    best seller list view item part one

--- lib/features/home/presentation/views/widget/home_view_body.dart
+++ lib/features/home/presentation/views/widget/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,6 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
         FeaturedListView(),
@@ -18,12 +18,41 @@ class HomeViewBody extends StatelessWidget {
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 16),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text('Best Seller', style: Styles.titleMeduim),
+              SizedBox(height: 20),
+              BestSellerListViewItem()
             ],
           ),
         ),
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.6 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                image: DecorationImage(
+                    image: AssetImage(AssetsData.testImage), fit: BoxFit.fill),
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
