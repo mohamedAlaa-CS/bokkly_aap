a9b271696df0e00d0eb70f54390ed7d603768191
Myproject <mohamed.alaa@gmail.com>
2023-10-23 23:38:36 -0700

    add styles file and text in home& goggle font

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class Styles {
+  static TextStyle titleMeduim = GoogleFonts.montserrat(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+}
--- lib/features/home/presentation/views/widget/home_view_body.dart
+++ lib/features/home/presentation/views/widget/home_view_body.dart
@@ -1,32 +1,29 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
+import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [CustomAppBar(), FeaturedListView()],
-    );
-  }
-}
-
-class FeaturedListView extends StatelessWidget {
-  const FeaturedListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .25,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) => const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8),
-                child: FeaturedListViewItem(),
-              )),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const CustomAppBar(),
+        const FeaturedListView(),
+        const SizedBox(height: 40),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Text('Best Seller', style: Styles.titleMeduim),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
