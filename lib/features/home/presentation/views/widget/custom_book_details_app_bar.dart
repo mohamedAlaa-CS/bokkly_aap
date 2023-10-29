5b5c3d0ba7f9941e7570fc9f7b82d5eccc164308
Myproject <mohamed.alaa@gmail.com>
2023-10-29 15:48:21 +0200

    finish ui

--- lib/features/home/presentation/views/widget/custom_book_details_app_bar.dart
+++ lib/features/home/presentation/views/widget/custom_book_details_app_bar.dart
@@ -9,7 +9,9 @@ class CustomBookDetailsAppBar extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
             icon: const Icon(
               Icons.close,
               size: 30,
