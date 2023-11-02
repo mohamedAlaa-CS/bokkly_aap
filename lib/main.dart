2d4607137ce5c8e8c7b797947df07ed50a66df80
Myproject <mohamed.alaa@gmail.com>
2023-11-02 11:12:54 +0200

    setUp service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:bookly/constants.dart';
-import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
 import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly/features/home/presentation/manager/featured_books/featured_books_cubit.dart';
 import 'package:bookly/features/home/presentation/manager/newset_books/newset_books_cubit.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setUpServiceLocator();
   runApp(const MyApp());
 }
 
@@ -22,13 +22,11 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) =>
-              FeaturedBooksCubit(HomeRepoImpl(ApiService(Dio())))
-                ..fetchFeauredBooks(),
+          create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>())
+            ..fetchFeauredBooks(),
         ),
         BlocProvider(
-          create: (context) =>
-              NewsetBooksCubit(HomeRepoImpl(ApiService(Dio()))),
+          create: (context) => NewsetBooksCubit(getIt.get<HomeRepoImpl>()),
         )
       ],
       child: MaterialApp.router(
