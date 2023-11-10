9d1f2851e8c0920f54674b3bc32a6a9183ec67ef
Myproject <mohamed.alaa@gmail.com>
2023-11-10 23:35:32 +0200

    handel miss query issue

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -55,7 +55,7 @@ class HomeRepoImpl implements HomeRepo {
     try {
       var data = await apiService.get(
         endPoint:
-            'volumes?Filtering=free-ebooks&Sorting=relevance &q=$category',
+            'volumes?Filtering=free-ebooks&Sorting=relevance &q=subject:$category',
       );
 
       List<BookModel> books = [];
